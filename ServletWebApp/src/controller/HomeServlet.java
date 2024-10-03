476e3531003d7786cb47ea5f0fa56af1f35e0ee8
Serge Mezui <sergemezui.dev@gmail.com>
2024-10-03 20:13:07 +0200

    add gome servlet

---
+++ ServletWebApp/src/controller/HomeServlet.java
@@ -0,0 +1,42 @@
+package controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import service.CookiesService;
+
+/**
+ * Servlet implementation class Home
+ */
+@WebServlet("/home")
+public class HomeServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HomeServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		Cookie[] cookies = CookiesService.getCookies(request);
+		
+		if(cookies == null) {
+			response.sendRedirect("login?message=NotLogged");
+			return;
+		}
+		
+		request.getRequestDispatcher("/index.html").forward(request, response);
+	}
+
+}
